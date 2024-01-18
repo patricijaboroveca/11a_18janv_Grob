2f577f2622225dca690bc9482bd7d5437edd63ea
patricijaboroveca <patricija.boroveca@grobinasskola.lv>
2024-01-18 15:10:20 +0200

    Init commit

---
+++ 3uzd.py
@@ -0,0 +1,8 @@
+
+file_path = 'aa.txt'  
+with open(file_path, 'r', encoding="utf-8") as file:
+    lines = file.readlines()
+    if len(lines) >= 3:
+        print(f"Trešā rinda: {lines[2]}")
+    else:
+        print("Failā nav pietiekami daudz rindu.")
